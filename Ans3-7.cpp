9e8379fa831c7fb52a9400747ca0feb9ec2a47e1
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-09-26 17:16:10 +0530

    Update Ans3-7.cpp

--- Ans3-7.cpp
+++ Ans3-7.cpp
@@ -51,30 +51,26 @@ public:
               if (month < 1 || month > 12)
                      return false;
 
-              // Check if the day is valid
               if (day < 1 || day > 31)
                      return false;
 
-              // Check for specific month-day combinations that are invalid
               if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
                      return false;
 
               if (month == 2)
               {
-                     // Check for leap year
-                     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                 if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
                      {
                             if (day > 29)
-                                   return false;
+                               return false;
                      }
                      else
                      {
-                            if (day > 28)
-                                   return false;
+                         if (day > 28)
+                             return false;
                      }
               }
 
-              // Date is valid
               return true;
        }
 };
