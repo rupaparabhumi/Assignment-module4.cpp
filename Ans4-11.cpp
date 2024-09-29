2da4b798c1cb04530346db4d859165b9f27ec646
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-09-29 14:22:36 +0530

    Add files via upload

---
+++ Ans4-11.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Calculate
+{
+public:
+        float pi = 3.14;
+        float AreaCricle(float r)
+        {
+                float AreaCricle;
+                AreaCricle = pi * r * r;
+                return AreaCricle;
+        }
+
+        float AreaRectangle(float l, float b)
+        {
+                float AreaRectangle;
+                AreaRectangle = l * b;
+                return AreaRectangle;
+        }
+        float AreaTriangle(float b, float h)
+        {
+                float AreaTriangle;
+                AreaTriangle = b * h * 1 / 2;
+                return AreaTriangle;
+        }
+};
+int main()
+{
+        float r, l, b, breadth, h;
+
+        cout << "Enter your r:";
+        cin >> r;
+        cout << "Enter your l:";
+        cin >> l;
+        cout << "Enter your b:";
+        cin >> b;
+        // cout << "Enter your breadth:";
+        // cin >> breadth;
+        cout << "Enter your h:";
+        cin >> h;
+        Calculate C1;
+        cout << "--------------------" << endl;
+        cout << "Area of circle:" << C1.AreaCricle(r) << endl;
+        cout << "Area of Rectangle:" << C1.AreaRectangle(l, b) << endl;
+        cout << "Area of Triangle:" << C1.AreaTriangle(b, h) << endl;
+}
