e7ec0cb5e6dbcd71ed557e75e1ceed3258326d39
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-10-16 21:15:12 +0530

    Update Ans5-2.cpp

--- Ans5-2.cpp
+++ Ans5-2.cpp
@@ -4,40 +4,40 @@ using namespace std;
 #define N 5
 template <class T>
 
-void sort<T arr,int size>
+void sort(T arr[], int size)
 {
-  for(int i=0;i<size;i++)
- {
-  for(int j=i+1;j<size;i++)
-  {
-  if(arr[i]>arr[j])
-   {
-    T temp;
-    temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-   }
-  }
- }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                T temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 int main()
 {
-  int array[N];
+    int int_array[N];
 
-cout<<"Enter your sort array:";
-for(int i=0;i<N;i++)
-{
-  cin>>array[i];
-}
+    cout << "Enter your sort array =" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> int_array[i];
+    }
 
-sort(array,N);
+    sort(int_array, N);
 
-cout<<"sort the array:";
-for(int i=0;i<N;i++)
-{
-  cout<<array[i]<<",";
-}
+    cout << "sort the array =" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << int_array[i] << ", ";
+    }
 
-return 0;
+    return 0;
 }
